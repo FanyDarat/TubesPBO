2d73ef8d31fdd9ec1db1957b1ebd336eb0ec9e52
dellasilviraa <silviradella03@gmail.com>
2024-12-27 17:43:04 +0700

    Della Cantik

--- src/Login.java
+++ src/Login.java
@@ -1,54 +1,52 @@
 import java.awt.*;
 import javax.swing.*;
 /*
- * Created by JFormDesigner on Thu Dec 26 13:01:00 WIB 2024
+ * Created by JFormDesigner on Fri Dec 27 17:41:36 ICT 2024
  */
 
 
 
 /**
- * @author rafae
+ * @author Dellass
  */
-public class Register extends JFrame {
-    public Register() {
+public class Login extends JPanel {
+    public Login() {
         initComponents();
     }
 
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
         // Generated using JFormDesigner Evaluation license - Della Silvira
-        button1 = new JButton();
 
         //======== this ========
-        var contentPane = getContentPane();
-        contentPane.setLayout(null);
-
-        //---- button1 ----
-        button1.setText("Register");
-        contentPane.add(button1);
-        button1.setBounds(new Rectangle(new Point(155, 190), button1.getPreferredSize()));
+        setBorder ( new javax . swing. border .CompoundBorder ( new javax . swing. border .TitledBorder (
+        new javax . swing. border .EmptyBorder ( 0, 0 ,0 , 0) ,  "JFor\u006dDesi\u0067ner \u0045valu\u0061tion"
+        , javax. swing .border . TitledBorder. CENTER ,javax . swing. border .TitledBorder . BOTTOM
+        , new java. awt .Font ( "Dia\u006cog", java .awt . Font. BOLD ,12 )
+        ,java . awt. Color .red ) , getBorder () ) );  addPropertyChangeListener(
+        new java. beans .PropertyChangeListener ( ){ @Override public void propertyChange (java . beans. PropertyChangeEvent e
+        ) { if( "bord\u0065r" .equals ( e. getPropertyName () ) )throw new RuntimeException( )
+        ;} } );
+        setLayout(null);
 
         {
             // compute preferred size
             Dimension preferredSize = new Dimension();
-            for(int i = 0; i < contentPane.getComponentCount(); i++) {
-                Rectangle bounds = contentPane.getComponent(i).getBounds();
+            for(int i = 0; i < getComponentCount(); i++) {
+                Rectangle bounds = getComponent(i).getBounds();
                 preferredSize.width = Math.max(bounds.x + bounds.width, preferredSize.width);
                 preferredSize.height = Math.max(bounds.y + bounds.height, preferredSize.height);
             }
-            Insets insets = contentPane.getInsets();
+            Insets insets = getInsets();
             preferredSize.width += insets.right;
             preferredSize.height += insets.bottom;
-            contentPane.setMinimumSize(preferredSize);
-            contentPane.setPreferredSize(preferredSize);
+            setMinimumSize(preferredSize);
+            setPreferredSize(preferredSize);
         }
-        pack();
-        setLocationRelativeTo(getOwner());
         // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
     // Generated using JFormDesigner Evaluation license - Della Silvira
-    private JButton button1;
     // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
 }
