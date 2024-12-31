0d0f3eae6c7a8d6e5fa389cdb4c98900ac38d32f
rafaelabednego123 <rafaelabednego123@gmail.com>
2024-12-31 07:18:43 +0000

    Database Online

--- src/DatabaseUtil.java
+++ src/DatabaseUtil.java
@@ -3,11 +3,11 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseUtil {
-    private static final String URL = "jdbc:mysql://127.0.0.1:3308/TubesPBO";
-    private static final String USER = "root";
-    private static final String PASSWORD = "rafael123";
+    private static final String URL = "jdbc:mysql://mysql-703314b-rafaelabednego123-d6c9.e.aivencloud.com:14376/tubespbo";
+    private static final String USER = "avnadmin";
+    private static final String PASSWORD = "AVNS_tDl-qGjjb2oSl3y_i0m";
 
-    public static Connection getConnection() throws SQLException {
+    public static Connection getConnection() throws SQLException {  
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
